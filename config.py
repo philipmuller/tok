88993f07a9f3b145df582a95bc74d8ef0b5d1bd7
Philip Müller <philipmuller97@gmail.com>
2023-03-11 21:55:14 +0100

    Add board type to config file

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import keychain
 
 openai_key = keychain.openai_key
 
+board_type = "Arduino Leonardo" # Change to your board type
 board_config = "A potentiometer is connected to analog pin A0. A flex sensor connected to analog pin A1. A photoresistor is connected to analog pin A2. A button is connected to pin 3: the pin reads HIGH when the button is pressed. A piezoelectric speaker is connected to pin 8: don't use external libraries to play sounds on this speaker. A servomotor is connected to pin 10. A yellow LED is connected to pin 11, a green LED to pin 12, and a red LED to pin 13."
-system_prompt = "You are assisting a human with writing Arduino code. The Arduino is a Arduino Leonardo. You always have to output complete arduino code without additional text. Don't reply with anything but the code itself. Don't talk to the user directly, don't explain your code, just output the code itself. This is the request from the user:\n"
+system_prompt = f"You are assisting a human with writing Arduino code. The Arduino is a {board_type}. You always have to output complete arduino code without additional text. Don't reply with anything but the code itself. Don't talk to the user directly, don't explain your code, just output the code itself. This is the request from the user:\n"
